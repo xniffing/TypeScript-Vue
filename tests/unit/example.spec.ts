5295c72ba107894f044991e37f1bb7e77755d4ac
David Joao Da Silva Vicente <david.vicente@siemens.com>
2020-05-08 19:52:06 +0100

    Jest tests 😇

--- tests/unit/example.spec.ts
+++ tests/unit/example.spec.ts
@@ -1,5 +1,7 @@
 import { shallowMount } from "@vue/test-utils";
 import HelloWorld from "@/components/HelloWorld.vue";
+import HeroSection from "@/components/HeroSection.vue";
+import { gradientColor, textColor } from "@/plugins/Chroma";
 
 describe("HelloWorld.vue", () => {
   it("renders props.msg when passed", () => {
@@ -8,5 +10,27 @@ describe("HelloWorld.vue", () => {
       propsData: { msg }
     });
     expect(wrapper.text()).toMatch(msg);
+  });
+});
+
+describe("HeroSection.vue", () => {
+  it("renders props.title && props.description when passed", () => {
+    const title = "new title";
+    const description = "new description";
+    const wrapperTitle = shallowMount(HeroSection, {
+      propsData: { title }
+    });
+    const wrapperDesc = shallowMount(HeroSection, {
+      propsData: { description }
+    });
+    expect(wrapperTitle.text()).toMatch(title);
+    expect(wrapperDesc.text()).toMatch(description);
+  });
+});
+
+describe("Chroma.ts", () => {
+  it("Calculates correct contrast ratio", () => {
+    expect(gradientColor).toBeDefined();
+    expect(textColor).toBeDefined();
   });
 });
